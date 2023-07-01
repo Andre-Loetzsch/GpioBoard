545ae89f86ccc2b87282a1191e75e9bc7c8d020a
André Lötzsch <86790735+Andre-Loetzsch@users.noreply.github.com>
2023-07-01 15:37:32 +0200

    1 create a bouncetime behavior (#3)
    * Ready to test
    
    * Add logging
    
    * fixed bugs
    
    * Log the pin status change
    
    * bouncetime-behavior created

--- Oleander.GpioBoard/src/Board.cs
+++ Oleander.GpioBoard/src/Board.cs
@@ -1,131 +1,168 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using Oleander.Extensions.Logging.Abstractions;
 using System.Device.Gpio;
-using System.Threading;
 using System.Threading.Tasks;
+using Timer = System.Threading.Timer;
+using System.IO;
 
-namespace Oleander.GpioBoard
+namespace Oleander.GpioBoard;
+// https://learn.microsoft.com/de-de/dotnet/iot/tutorials/gpio-input?source=recommendations
+// https://learn.microsoft.com/de-de/dotnet/iot/debugging?tabs=self-contained&pivots=vscode
+
+public class Board
 {
-    // https://learn.microsoft.com/de-de/dotnet/iot/tutorials/gpio-input?source=recommendations
-    // https://learn.microsoft.com/de-de/dotnet/iot/debugging?tabs=self-contained&pivots=vscode
+    private const int outPin02 = 02;
+    private const int outPin03 = 03;
+    private const int outPin04 = 04;
+    private const int outPin05 = 05;
+    private const int outPin06 = 06;
+    private const int outPin07 = 07;
+    private const int outPin08 = 08;
+    private const int outPin09 = 09;
+    private const int outPin10 = 10;
+    private const int outPin11 = 11;
+    private const int outPin12 = 12;
+    private const int outPin13 = 13;
+    private const int outPin14 = 14;
+    private const int outPin15 = 15;
+    private const int outPin16 = 16;
+    private const int outPin17 = 17;
 
-    public class Board
+
+    private readonly InputPin _inPin18;
+    private readonly InputPin _inPin19;
+    private readonly InputPin _inPin20;
+    private readonly InputPin _inPin21;
+    private readonly InputPin _inPin22;
+    private readonly InputPin _inPin23;
+    private readonly InputPin _inPin24;
+    private readonly InputPin _inPin25;
+    private readonly InputPin _inPin26;
+    private readonly InputPin _inPin27;
+
+
+    private readonly Dictionary<int, InputPin> _inputPins = new();
+
+    private string[] _inpstatus = new string[10];
+
+    private int _currentOutPin = 01;
+
+    private int _inPinCounter;
+    private int _lastInPin;
+
+    private const string alert = "ALERT 🚨";
+    private const string ready = "READY ✅";
+
+    private Timer? _puseTimer;
+    private bool _pulseValue;
+
+    private readonly GpioController _controller = new();
+
+    public Board()
     {
-        private const int outPin02 = 02;
-        private const int outPin03 = 03;
-        private const int outPin04 = 04;
-        private const int outPin05 = 05;
-        private const int outPin06 = 06;
-        private const int outPin07 = 07;
-        private const int outPin08 = 08;
-        private const int outPin09 = 09;
-        private const int outPin10 = 10;
-        private const int outPin11 = 11;
-        private const int outPin12 = 12;
-        private const int outPin13 = 13;
-        private const int outPin14 = 14;
-        private const int outPin15 = 15;
-        private const int outPin16 = 16;
-        private const int outPin17 = 17;
+        this.Logger = LoggerFactory.CreateLogger<Board>();
 
+        this._inPin18 = new InputPin(this.Logger, 18);
+        this._inPin19 = new InputPin(this.Logger, 19); // rot
+        this._inPin20 = new InputPin(this.Logger, 20); // ws
+        this._inPin21 = new InputPin(this.Logger, 21); // ge
+        this._inPin22 = new InputPin(this.Logger, 22); // ws
+        this._inPin23 = new InputPin(this.Logger, 23); // gn
+        this._inPin24 = new InputPin(this.Logger, 24); // ws
+        this._inPin25 = new InputPin(this.Logger, 25); // br
+        this._inPin26 = new InputPin(this.Logger, 26); // rt
+        this._inPin27 = new InputPin(this.Logger, 27); // sw
 
-        private const int inPin18 = 18;
-        private const int inPin19 = 19;
-        private const int inPin20 = 20;
-        private const int inPin21 = 21;
-        private const int inPin22 = 22;
-        private const int inPin23 = 23;
-        private const int inPin24 = 24;
-        private const int inPin25 = 25;
-        private const int inPin26 = 26;
-        private const int inPin27 = 27;
+    }
 
-        private int _currentOutPin = 01;
+    internal readonly ILogger Logger;
 
-
-
-        private int _inPinCounter;
-        private int _lastInPin;
-
-        private const string alert = "ALERT 🚨";
-        private const string ready = "READY ✅";
-
-        private Timer? _puseTimer;
-        private bool _pulseValue;
-
-        private readonly GpioController _controller = new();
-
-        public Board()
+    public async Task StartAsync()
+    {
+        await Task.Run(() =>
         {
-            this.Logger = LoggerFactory.CreateLogger<Board>();
-        }
-
-        internal readonly ILogger Logger;
-
-        public async Task StartAsync()
-        {
-            await Task.Run(() =>
+            try
             {
-                try
+
+                File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RawInputs"), "");
+
+                this._controller.OpenPin(outPin02, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin03, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin04, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin05, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin06, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin07, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin08, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin09, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin10, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin11, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin12, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin13, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin14, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin15, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin16, PinMode.Output, PinValue.High);
+                this._controller.OpenPin(outPin17, PinMode.Output, PinValue.High);
+
+
+                this._inputPins.Add(this._inPin18.PinNumber, this._inPin18);
+                this._inputPins.Add(this._inPin19.PinNumber, this._inPin19);
+                this._inputPins.Add(this._inPin20.PinNumber, this._inPin20);
+                this._inputPins.Add(this._inPin21.PinNumber, this._inPin21);
+                this._inputPins.Add(this._inPin22.PinNumber, this._inPin22);
+                this._inputPins.Add(this._inPin23.PinNumber, this._inPin23);
+                this._inputPins.Add(this._inPin24.PinNumber, this._inPin24);
+                this._inputPins.Add(this._inPin25.PinNumber, this._inPin25);
+                this._inputPins.Add(this._inPin26.PinNumber, this._inPin26);
+                this._inputPins.Add(this._inPin27.PinNumber, this._inPin27);
+
+                foreach (var inputPins in this._inputPins.Values)
                 {
-                    this._controller.OpenPin(outPin02, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin03, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin04, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin05, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin06, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin07, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin08, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin09, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin10, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin11, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin12, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin13, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin14, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin15, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin16, PinMode.Output, PinValue.High);
-                    this._controller.OpenPin(outPin17, PinMode.Output, PinValue.High);
+                    this._inpstatus[inputPins.PinNumber - 18] = string.Empty;
 
-
-                    this._controller.OpenPin(inPin18, PinMode.InputPullDown, PinValue.High);
-                    this._controller.OpenPin(inPin19, PinMode.InputPullDown, PinValue.High);
-                    this._controller.OpenPin(inPin20, PinMode.InputPullDown, PinValue.High);
-                    this._controller.OpenPin(inPin21, PinMode.InputPullDown, PinValue.High);
-                    this._controller.OpenPin(inPin22, PinMode.InputPullDown, PinValue.High);
-                    this._controller.OpenPin(inPin23, PinMode.InputPullDown, PinValue.High);
-                    this._controller.OpenPin(inPin24, PinMode.InputPullDown, PinValue.High);
-                    this._controller.OpenPin(inPin25, PinMode.InputPullDown, PinValue.High);
-                    this._controller.OpenPin(inPin26, PinMode.InputPullDown, PinValue.High);
-                    this._controller.OpenPin(inPin27, PinMode.InputPullDown, PinValue.High);
-
-
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin18, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin19, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin20, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin21, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin22, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin23, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin24, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin25, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin26, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-                    this._controller.RegisterCallbackForPinValueChangedEvent(inPin27, PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
-
-
-                    this._puseTimer = new Timer(state =>
+                    inputPins.ValueChanged += args =>
                     {
-                        //this._controller.Write(outPin02, this._pulseValue ? PinValue.High : PinValue.Low);
+                        this._inpstatus[args.PinNumber - 18] = $"{DateTime.Now:yyyy.MM.dd HH:mm:ss fff} {args.PinNumber}:{(args.ChangeType is PinEventTypes.Rising ? "1" : "0")}";
+                        File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Inputs"), string.Join(Environment.NewLine, this._inpstatus));
+
+                        var logMsg = this._lastInPin != args.PinNumber ? string.Concat("==============================================", Environment.NewLine) : string.Empty;
+                        logMsg = string.Concat(logMsg, $"Pin={args.PinNumber}, State={(args.ChangeType is PinEventTypes.Rising ? alert : ready)}");
+                        this.Logger.LogInformation(logMsg);
+
+                        this._lastInPin = args.PinNumber;
+                    };
+
+                    if (inputPins.PinNumber == 19)
+                    {
+                        this._controller.OpenPin(inputPins.PinNumber, PinMode.Input, PinValue.Low);
+                    }
+                    else
+                    {
+                        this._controller.OpenPin(inputPins.PinNumber, PinMode.InputPullDown, PinValue.Low);
+                    }
+
+                    this._controller.RegisterCallbackForPinValueChangedEvent(inputPins.PinNumber,
+                            PinEventTypes.Falling | PinEventTypes.Rising, this.OnPinEvent);
+                    
+                }
+
+
+                this._puseTimer = new Timer(state =>
+                {
+                    try
+                    {
                         this._pulseValue = !this._pulseValue;
 
                         if (this._inPinCounter != 0)
                         {
                             if (this._inPinCounter > 10)
                             {
-                                this.Logger.LogInformation("Counter 1: Pin={pin}, Count={count}", inPin18, this._inPinCounter);
+                                this.Logger.LogInformation("inPinCounter={count}", this._inPinCounter);
                             }
 
                             this._inPinCounter = 0;
                         }
-
 
                         if (this._currentOutPin < 2)
                         {
@@ -143,62 +180,65 @@ namespace Oleander.GpioBoard
                         {
                             this._currentOutPin = 1;
                         }
+                    }
+                    catch (Exception ex)
+                    {
+                        this.Logger.LogError("useTimer caused an exception! {ex}", ex.GetAllMessages());
+                    }
 
 
-                    }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+                }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
 
-                }
-                catch (Exception ex)
-                {
-                    this.Logger.LogError("Start failed! {ex}", ex.GetAllMessages());
-                }
-
-            });
-
-        }
-
-        public async Task StopAsync()
-        {
-
-            await Task.Run(() =>
+            }
+            catch (Exception ex)
             {
-                try
-                {
-                    this._controller.Dispose();
-                }
-                catch (Exception ex)
-                {
-                    this.Logger.LogError("Stop failed! {ex}", ex.GetAllMessages());
-                }
-            });
-        }
-
-
-        private void OnPinEvent(object sender, PinValueChangedEventArgs args)
-        {
-            if (args.ChangeType != PinEventTypes.Rising)
-            {
-                this._inPinCounter++;
+                this.Logger.LogError("Start failed! {ex}", ex.GetAllMessages());
             }
 
-            //if (this._inPinCounter > 10)
-            //{
-            //    return;
-            //}
+        });
 
-            if (this._lastInPin != args.PinNumber)
+    }
+
+    public async Task StopAsync()
+    {
+
+        await Task.Run(() =>
+        {
+            try
             {
-                this.Logger.LogInformation("==============================================");
-                this._lastInPin = args.PinNumber;
+                this._controller.Dispose();
             }
+            catch (Exception ex)
+            {
+                this.Logger.LogError("Stop failed! {ex}", ex.GetAllMessages());
+            }
+        });
+    }
 
-            this.Logger.LogInformation("Pin={pin}, State={state}", args.PinNumber, args.ChangeType is PinEventTypes.Rising ? alert : ready);
 
-            if (args.ChangeType != PinEventTypes.Rising) return;
+    private void OnPinEvent(object sender, PinValueChangedEventArgs args)
+    {
+        if (args.ChangeType == PinEventTypes.Rising) this._inPinCounter++;
 
-            //var value = this._controller.Read(outPin02);
-            //this._controller.Write(outPin02, value == PinValue.Low ? PinValue.High : PinValue.Low);
+
+        if (!this._inputPins.TryGetValue(args.PinNumber, out var inputPin))
+        {
+            this.Logger.LogWarning("Pin {pin} does not exist! State={state}", args.PinNumber, args.ChangeType is PinEventTypes.Rising ? alert : ready);
+            return;
         }
 
+        //this.Logger.LogInformation("Pin {pin}, State={state}", args.PinNumber, args.ChangeType is PinEventTypes.Rising ? alert : ready);
+
+        try
+        {
+            inputPin.CallbackForPinValueChangedEvent(sender, args);
+
+            File.AppendAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RawInputs"),
+                $"{DateTime.Now:yyyy.MM.dd HH:mm:ss fff}:{args.PinNumber}:{args.ChangeType}{Environment.NewLine}");
+        }
+        catch (Exception ex)
+        {
+            this.Logger.LogError("OnPinEvent caused an exception! {ex}", ex.GetAllMessages());
+        }
     }
 }
---
+++ Oleander.GpioBoard/src/InputPin.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Linq;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Oleander.Extensions.Logging.Abstractions;
+
+namespace Oleander.GpioBoard;
+
+internal class InputPin : IDisposable
+{
+    private readonly ILogger _logger;
+    public event Action<PinValueChangedEventArgs>? ValueChanged;
+
+    private static readonly Dictionary<int, InputPin> inputPins = new();
+    private static Timer? timer;
+
+    private readonly List<int> _pinChanges = new();
+    private bool _lastValue;
+
+    public InputPin(ILogger logger, int pinNumber, string? name = null)
+    {
+        if (pinNumber is < 18 or > 27)
+            throw new ArgumentOutOfRangeException(nameof(pinNumber), pinNumber, "The PIN number must be between 18 and 27!");
+        this._logger = logger;
+
+        this.PinNumber = pinNumber;
+        this.Name = name ?? $"Pin {pinNumber}";
+
+        if (inputPins.ContainsKey(this.PinNumber))
+            throw new ArgumentException($"Pin {pinNumber} already existed!", nameof(pinNumber));
+
+        inputPins.Add(this.PinNumber, this);
+
+        timer ??= new(_ =>
+        {
+            foreach (var inputPin in inputPins.Values)
+            {
+                try
+                {
+                    inputPin.PublishValue();
+                }
+                catch (Exception ex)
+                {
+                    this._logger.LogError("PublishValue caused an exception! {ex}", ex.GetAllMessages());
+                }
+            }
+
+        }, null, TimeSpan.FromMilliseconds(100), TimeSpan.FromMilliseconds(100));
+
+    }
+
+    public DateTime BounceTime { get; private set; } = DateTime.Now;
+
+    public TimeSpan BounceTimeSpan { get; set; } = TimeSpan.FromMicroseconds(200);
+
+    public int PinNumber { get; }
+
+    public bool Value { get; private set; }
+
+    public string Name { get; }
+
+    public void CallbackForPinValueChangedEvent(object sender, PinValueChangedEventArgs args)
+    {
+        if (this.PinNumber != args.PinNumber) return;
+
+        lock (this._pinChanges)
+        {
+            this._pinChanges.Add(args.ChangeType == PinEventTypes.Rising ? 1 : 0);
+        }
+
+        if (this._lastValue && args.ChangeType == PinEventTypes.Rising) return;
+
+        //this._logger.LogInformation("CallbackForPinValueChangedEvent: {PinNumber}", this.PinNumber);
+
+        this.BounceTime = DateTime.Now.Add(this.BounceTimeSpan);
+        this._lastValue = args.ChangeType == PinEventTypes.Rising;
+    }
+
+    private void PublishValue()
+    {
+        if (this.Value == this._lastValue) return;
+        if ((DateTime.Now - this.BounceTime).TotalMicroseconds < 1) return;
+
+        lock (this._pinChanges)
+        {
+            var pinChangedList = this._pinChanges.ToList();
+            this._logger.LogInformation("Pin={pin}, PublishValue={value} -> {lastValue} {pinChanges}", this.PinNumber,
+                this.Value, this._lastValue, string.Join(":", pinChangedList));
+
+            this._pinChanges.Clear();
+        }
+
+        this.Value = this._lastValue;
+        this.ValueChanged?.Invoke(new(this.Value ? PinEventTypes.Rising : PinEventTypes.Falling, this.PinNumber));
+    }
+
+    #region IDisposable
+
+    private void ReleaseThis()
+    {
+        inputPins.Remove(this.PinNumber);
+
+        if (inputPins.Count > 0) return;
+        timer?.Dispose();
+    }
+
+    public void Dispose()
+    {
+        this.ReleaseThis();
+        GC.SuppressFinalize(this);
+    }
+
+    ~InputPin()
+    {
+        this.ReleaseThis();
+    }
+
+    #endregion
+} 
